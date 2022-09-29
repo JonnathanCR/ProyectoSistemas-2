41858f19998099dfd9d2b0f42497bc0fd7211f66
JonnathanCR <72930023+JonnathanCR@users.noreply.github.com>
2022-09-29 11:15:01 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,48 @@
+// Create a tile layer from OSM
+var osmLayer = new ol.layer.Tile({
+  source: new ol.source.OSM()
+});
+osmLayer.set('name', 'osm');
+
+// Define style for geojsonLayer
+var styles = {
+  'Polygon': [new ol.style.Style({
+      stroke: new ol.style.Stroke({
+          color: 'blue',
+          lineDash: [4],
+          width: 3
+      })
+  })]
+};
+
+var styleFunction = function(feature, resolution) {
+  return styles[feature.getGeometry().getType()];
+};
+
+// Create geojsonLayer
+var geojsonLayer = new ol.layer.Vector({
+    source: new ol.source.Vector({
+        url: 'file.geojson',
+        format: new ol.format.GeoJSON({
+          // Transforms geojson data from EPSG:2039 to EPSG:3857
+          defaultDataProjection: 'EPSG:2039',
+          projection: 'EPSG:3857'
+        })
+    }),
+    style: styleFunction
+});
+
+// Coordinates of a center are in EPSG:4326, they are being transform in EPSG:3857
+// http://openlayers.org/en/v3.8.2/apidoc/ol.proj.html
+var olview = new ol.View({
+    center: ol.proj.fromLonLat([34.7621407, 32.0878858]),
+    zoom: 13,
+    minZoom: 2,
+    maxZoom: 20
+});
+
+var map = new ol.Map({
+    target: document.getElementById('map'),
+    view: olview,
+    layers: [osmLayer, geojsonLayer]
+});
